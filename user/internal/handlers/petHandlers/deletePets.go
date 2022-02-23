ee9fd6338df3eefc67999b8474205ea539f0a061
visbm <visbm@mail.ru>
2022-02-23 14:16:43 +0300

    first

--- user/internal/handlers/petHandlers/deletePets.go
+++ user/internal/handlers/petHandlers/deletePets.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"user/internal/apperror"
 	"user/internal/store"
 	"user/pkg/response"
 
@@ -19,22 +20,22 @@ func DeletePets(s *store.Store) httprouter.Handle {
 		id, err := strconv.Atoi(ps.ByName("id"))
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			s.Logger.Errorf("Bad request. Err msg:%v. Requests body: %v", err, r.FormValue("id"))			
+			s.Logger.Errorf("Bad request. Err msg:%v. Requests body: %v", err, r.FormValue("id"))
+			json.NewEncoder(w).Encode(apperror.NewAppError(fmt.Sprintf("Bad request. Err msg:%v. Requests body: %v", err, ps.ByName("id")), fmt.Sprintf("%d", http.StatusInternalServerError), fmt.Sprintf("Bad request. Err msg:%v. Requests body: %v", err, ps.ByName("id"))))
 			return
 		}
 		err = s.Open()
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			s.Logger.Errorf("Can't open DB. Err msg:%v.", err)
+			json.NewEncoder(w).Encode(apperror.NewAppError("Can't open DB", fmt.Sprintf("%d", http.StatusInternalServerError), fmt.Sprintf("Can't open DB. Err msg:%v.", err)))
 			return
 		}
 		err = s.Pet().Delete(id)
 		if err != nil {
 			log.Print(err)
-			s.Logger.Errorf("Can't delete pet. Err msg:%v.", err)
+			json.NewEncoder(w).Encode(apperror.NewAppError("Can't delete pet.", fmt.Sprintf("%d", http.StatusInternalServerError), fmt.Sprintf("Can't delete pet. Err msg:%v.", err)))
 			return
 		}
-		s.Logger.Info("Delete pet with id = %d", id)
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(response.Info{Messsage: fmt.Sprintf("Delete pet with id = %d", id)})
 
